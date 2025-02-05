c89c28f6998cb2621ea8586cc718c7c991e80cb6
amir-mc <amirfooladi93@gmail.com>
2025-02-05 11:15:22 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Alaki from "@/components/alaki";
+
+import Grid from "@/components/grid";
 import Hero from "@/components/hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
    
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+   <main className="relative bg-black-100 flex justify-center 
+   items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full ">
       <FloatingNav
         navItems={[
@@ -16,8 +19,10 @@ export default function Home() {
         ]}
       />
       <Hero />
-        <Alaki/>
+        <Grid/>
+
     </div>
+         
     </main>
    
   );
